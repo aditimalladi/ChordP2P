8abbcae3f1931f19748ebab4280058bc5deac258
Aditi Malladi <aditimalladi@gmail.com>
2018-10-14 10:40:36 -0400

    Initial commit

---
+++ lib/chordp2p.ex
@@ -0,0 +1,18 @@
+defmodule ChordP2P do
+  @moduledoc """
+  Documentation for ChordP2P.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ChordP2P.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/chordp2p_test.exs
@@ -0,0 +1,8 @@
+defmodule ChordP2PTest do
+  use ExUnit.Case
+  doctest ChordP2P
+
+  test "greets the world" do
+    assert ChordP2P.hello() == :world
+  end
+end
